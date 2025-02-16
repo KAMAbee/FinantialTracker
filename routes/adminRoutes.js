558fe4b5a9ea2d828d0f5785bfc0a7f37c30205d
KAMAbee <kamil.akbarov.94@gmail.com>
2025-02-17 03:37:17 +0500

    admin page start

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,8 +5,26 @@ const User = require('../models/User');
 const { authenticateJWT, verifyRole } = require('../middleware/authMiddleware');
 
 
-router.get('/', authenticateJWT, verifyRole, (req, res) => {
-    res.render('admin', { message: null });
+router.get('/', authenticateJWT, verifyRole, async (req, res) => {
+    const users = await User.find({});
+    res.render('admin', {users, message: null });
+});
+
+router.post('/updateRole', authenticateJWT, verifyRole, async (req, res) => {
+    try {
+        const { userId, role } = req.body;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.role = role;
+        await user.save();
+        res.redirect('/admin');
+    } catch (err) {
+        res.status(500).json({ error: 'Error updating role' });
+    }
 });
 
 module.exports = router;
