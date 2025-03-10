b37bcbcebddd41e2e415e78ff15f31e51c8389dd
KAMAbee <kamil.akbarov.94@gmail.com>
2025-03-10 22:04:47 +0500

    test

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,6 +3,7 @@ const errorHandler = (err, req, res, next) => {
     res.status(err.status || 500).json({
         success: false,
         message: err.message || 'Internal Server Error',
+        global: "global"
     });
 };
 
